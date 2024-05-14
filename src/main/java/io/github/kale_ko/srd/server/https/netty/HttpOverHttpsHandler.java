a3f4eee70c3e701f93a5101ae1f0e768b306e30f
Kale-Ko <github@kaleko.dev>
2024-05-13 23:35:15 -0400

    HttpOverHttpsHandler

---
+++ src/main/java/io/github/kale_ko/srd/server/https/netty/HttpOverHttpsHandler.java
@@ -0,0 +1,58 @@
+package io.github.kale_ko.srd.server.https.netty;
+
+import io.github.kale_ko.srd.server.https.HttpsServer;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class HttpOverHttpsHandler extends ChannelInboundHandlerAdapter {
+    protected final @NotNull HttpsServer parent;
+
+    public HttpOverHttpsHandler(@NotNull HttpsServer parent) {
+        this.parent = parent;
+    }
+
+    public @NotNull HttpsServer getParent() {
+        return this.parent;
+    }
+
+    @Override
+    public void channelRead(@NotNull ChannelHandlerContext ctx, @NotNull Object msg) {
+        if (msg instanceof ByteBuf buffer) {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < buffer.readableBytes(); i++) {
+                int read = buffer.getByte(i);
+
+                if (read == '\r' || read == '\n') {
+                    break;
+                }
+
+                if (Character.isLetterOrDigit(read) || Character.isWhitespace(read) || "-._~:/?#[]@!$&'()*+,;=".indexOf(read) != -1) {
+                    stringBuilder.appendCodePoint(read);
+                } else {
+                    break;
+                }
+            }
+
+            String string = stringBuilder.toString();
+            if (string.contains("HTTP")) {
+                ctx.fireChannelRead(new Message());
+
+                buffer.release();
+            } else {
+                ctx.fireChannelRead(buffer.copy());
+
+                buffer.release();
+            }
+        } else {
+            parent.getLogger().warn("Unknown type passed to {}, {}!", this.getClass().getSimpleName(), msg.getClass().getSimpleName());
+
+            ctx.fireChannelRead(msg);
+        }
+    }
+
+    public static class Message {
+    }
+}
