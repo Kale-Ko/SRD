771d7f281e3ff2596d91e6de7ff9e0dcc1f1c054
Kale-Ko <github@kaleko.dev>
2024-05-17 18:07:04 -0400

    HttpExceptionHandler

---
+++ src/main/java/io/github/kale_ko/srd/server/http/netty/HttpExceptionHandler.java
@@ -0,0 +1,30 @@
+package io.github.kale_ko.srd.server.http.netty;
+
+import io.github.kale_ko.srd.server.http.HttpServer;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import java.net.SocketException;
+import org.jetbrains.annotations.NotNull;
+
+public class HttpExceptionHandler extends ChannelInboundHandlerAdapter {
+    protected final @NotNull HttpServer parent;
+
+    public HttpExceptionHandler(@NotNull HttpServer parent) {
+        this.parent = parent;
+    }
+
+    public @NotNull HttpServer getParent() {
+        return this.parent;
+    }
+
+    @Override
+    public void exceptionCaught(@NotNull ChannelHandlerContext ctx, @NotNull Throwable exception) {
+        if (exception instanceof SocketException && exception.getMessage().equalsIgnoreCase("Connection reset")) {
+            ctx.close();
+        } else {
+            parent.getLogger().throwing(exception);
+
+            ctx.close();
+        }
+    }
+}
