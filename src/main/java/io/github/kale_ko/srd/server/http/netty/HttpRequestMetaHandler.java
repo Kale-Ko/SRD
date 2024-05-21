32d2b92a05f754443867a584f297ff8b6733bf31
Kale-Ko <github@kaleko.dev>
2024-05-20 23:01:17 -0400

    HttpRequestMetaHandler & HttpResponseMetaHandler

---
+++ src/main/java/io/github/kale_ko/srd/server/http/netty/HttpRequestMetaHandler.java
@@ -0,0 +1,48 @@
+package io.github.kale_ko.srd.server.http.netty;
+
+import io.github.kale_ko.srd.server.http.HttpServer;
+import io.github.kale_ko.srd.server.https.netty.HttpOverHttpsHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpRequest;
+import java.net.InetSocketAddress;
+import java.security.SecureRandom;
+import java.util.UUID;
+import org.jetbrains.annotations.NotNull;
+
+public class HttpRequestMetaHandler extends ChannelInboundHandlerAdapter {
+    protected final @NotNull HttpServer parent;
+
+    public HttpRequestMetaHandler(@NotNull HttpServer parent) {
+        this.parent = parent;
+    }
+
+    public @NotNull HttpServer getParent() {
+        return this.parent;
+    }
+
+    protected final @NotNull SecureRandom random = new SecureRandom();
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof HttpRequest request) {
+            UUID uuid = new UUID(random.nextLong(), random.nextLong());
+
+            request.headers().set("X-Request-Id", uuid);
+
+            if (ctx.channel().remoteAddress() instanceof InetSocketAddress address) {
+                request.headers().set("X-Request-Address", address.getAddress().getHostAddress() + ":" + address.getPort());
+                request.headers().set("X-Request-Ip", address.getAddress().getHostAddress());
+            } else {
+                parent.getLogger().error("[{}] Unknown socket address type, {}!", parent.getName(), ctx.channel().remoteAddress().getClass().getSimpleName());
+
+                request.headers().set("X-Request-Address", ctx.channel().remoteAddress().toString());
+            }
+        } else if (!(msg instanceof HttpContent || msg instanceof HttpOverHttpsHandler.Message)) {
+            parent.getLogger().warn("[{}] Unknown type passed to {}, {}!", parent.getName(), this.getClass().getSimpleName(), msg.getClass().getSimpleName());
+        }
+
+        ctx.fireChannelRead(msg);
+    }
+}
---
+++ src/main/java/io/github/kale_ko/srd/server/http/netty/HttpResponseMetaHandler.java
@@ -0,0 +1,34 @@
+package io.github.kale_ko.srd.server.http.netty;
+
+import io.github.kale_ko.srd.server.http.HttpServer;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpResponse;
+import org.jetbrains.annotations.NotNull;
+
+public class HttpResponseMetaHandler extends ChannelOutboundHandlerAdapter {
+    protected final @NotNull HttpServer parent;
+
+    public HttpResponseMetaHandler(@NotNull HttpServer parent) {
+        this.parent = parent;
+    }
+
+    public @NotNull HttpServer getParent() {
+        return this.parent;
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+        if (msg instanceof HttpResponse response) {
+            response.headers().remove("X-Request-Id");
+            response.headers().remove("X-Request-Address");
+            response.headers().remove("X-Request-Ip");
+        } else if (!(msg instanceof HttpContent)) {
+            parent.getLogger().warn("[{}] Unknown type passed to {}, {}!", parent.getName(), this.getClass().getSimpleName(), msg.getClass().getSimpleName());
+        }
+
+        ctx.write(msg, promise);
+    }
+}
