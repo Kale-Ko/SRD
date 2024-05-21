99319672a2165aa92fb2eb73b722d6bc802a484f
Kale-Ko <github@kaleko.dev>
2024-05-20 23:07:26 -0400

    Improve HttpLoggingHandler

--- src/main/java/io/github/kale_ko/srd/server/http/netty/HttpLoggingHandler.java
+++ src/main/java/io/github/kale_ko/srd/server/http/netty/HttpLoggingHandler.java
@@ -8,6 +8,7 @@ import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpStatusClass;
 import org.jetbrains.annotations.NotNull;
 
 public class HttpLoggingHandler extends ChannelDuplexHandler {
@@ -23,8 +24,10 @@ public class HttpLoggingHandler extends ChannelDuplexHandler {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        if (msg instanceof HttpRequest) {
-            parent.getLogger().trace("[{}] Read message: {}", parent.getName(), msg);
+        if (msg instanceof HttpRequest request) {
+            parent.getLogger().trace("[{}] Read message #{} from {}: {}", parent.getName(), request.headers().get("X-Request-Id"), request.headers().get("X-Request-Address"), request);
+
+            parent.getLogger().info("[{}] [{} #{}] {} {} {}", parent.getName(), request.headers().get("X-Request-Ip"), request.headers().get("X-Request-Id").split("-")[0], request.protocolVersion().text(), request.method().name(), request.uri());
         } else if (msg instanceof HttpOverHttpsHandler.Message) {
             parent.getLogger().trace("[{}] Read HttpOverHttps message", parent.getName());
         } else if (!(msg instanceof HttpContent)) {
@@ -36,8 +39,14 @@ public class HttpLoggingHandler extends ChannelDuplexHandler {
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
-        if (msg instanceof HttpResponse) {
-            parent.getLogger().trace("[{}] Wrote message: {}", parent.getName(), msg);
+        if (msg instanceof HttpResponse response) {
+            parent.getLogger().trace("[{}] Wrote message #{} to {}: {}", parent.getName(), response.headers().get("X-Request-Id"), response.headers().get("X-Request-Address"), response);
+
+            if (response.status().codeClass() == HttpStatusClass.SERVER_ERROR) {
+                parent.getLogger().warn("[{}] [{} #{}] {} {} {}", parent.getName(), response.headers().get("X-Request-Ip"), response.headers().get("X-Request-Id").split("-")[0], response.protocolVersion().text(), response.status().code(), response.status().reasonPhrase());
+            } else {
+                parent.getLogger().info("[{}] [{} #{}] {} {} {}", parent.getName(), response.headers().get("X-Request-Ip"), response.headers().get("X-Request-Id").split("-")[0], response.protocolVersion().text(), response.status().code(), response.status().reasonPhrase());
+            }
         } else if (!(msg instanceof HttpContent)) {
             parent.getLogger().warn("[{}] Unknown type passed to {}, {}!", parent.getName(), this.getClass().getSimpleName(), msg.getClass().getSimpleName());
         }
