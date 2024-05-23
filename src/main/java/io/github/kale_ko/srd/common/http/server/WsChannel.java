91bb54d31a558e345e6ad931960ef21934344194
Kale-Ko <github@kaleko.dev>
2024-05-22 22:57:13 -0400

    Allow accessing channel attributes in WsChannel

--- src/main/java/io/github/kale_ko/srd/common/http/server/WsChannel.java
+++ src/main/java/io/github/kale_ko/srd/common/http/server/WsChannel.java
@@ -3,9 +3,12 @@ package io.github.kale_ko.srd.common.http.server;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.util.Attribute;
+import io.netty.util.AttributeKey;
+import io.netty.util.AttributeMap;
 import org.jetbrains.annotations.NotNull;
 
-public class WsChannel {
+public class WsChannel implements AttributeMap {
     protected final @NotNull Channel channel;
 
     public WsChannel(@NotNull Channel channel) {
@@ -14,5 +17,15 @@ public class WsChannel {
 
     public void send(@NotNull WebSocketFrame frame) {
         channel.writeAndFlush(frame).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+    }
+
+    @Override
+    public <T> boolean hasAttr(AttributeKey<T> key) {
+        return channel.hasAttr(key);
+    }
+
+    @Override
+    public <T> Attribute<T> attr(AttributeKey<T> key) {
+        return channel.attr(key);
     }
 }
